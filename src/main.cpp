e4f1b48bca2f0e0209e39b8a73075c6ab7f4e37c
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-03-16 20:27:58 +0200

    treewide: Unify formatting
    Use clang-format on all c++ files.

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,34 @@
 #include <LayerShellQt6/shell.h>
 #include <LayerShellQt6/window.h>
 
-int main(int argc, char *argv[]) {
-    LayerShellQt::Shell::useLayerShell();
-    QGuiApplication app(argc, argv);
+int main(int argc, char *argv[])
+{
+	LayerShellQt::Shell::useLayerShell();
+	QGuiApplication app(argc, argv);
 
-    QString locale = QLocale::system().name();
-    QTranslator translator;
-    bool translated = translator.load(
-        QString(":/i18n/cutie-ongoing-call_") + locale);
-    if (translated)
-        app.installTranslator(&translator);
+	QString locale = QLocale::system().name();
+	QTranslator translator;
+	bool translated =
+		translator.load(QString(":/i18n/cutie-ongoing-call_") + locale);
+	if (translated)
+		app.installTranslator(&translator);
 
-    QQuickView view;
+	QQuickView view;
 
-    LayerShellQt::Window *layerShell = LayerShellQt::Window::get(&view);
-    layerShell->setLayer(LayerShellQt::Window::LayerOverlay);
-    layerShell->setAnchors(LayerShellQt::Window::AnchorTop);
-    layerShell->setKeyboardInteractivity(LayerShellQt::Window::KeyboardInteractivityExclusive);
-    layerShell->setExclusiveZone(-1);
-    layerShell->setScope("cutie-ongoing-call");
+	LayerShellQt::Window *layerShell = LayerShellQt::Window::get(&view);
+	layerShell->setLayer(LayerShellQt::Window::LayerOverlay);
+	layerShell->setAnchors(LayerShellQt::Window::AnchorTop);
+	layerShell->setKeyboardInteractivity(
+		LayerShellQt::Window::KeyboardInteractivityExclusive);
+	layerShell->setExclusiveZone(-1);
+	layerShell->setScope("cutie-ongoing-call");
 
-    QObject::connect(view.engine(), &QQmlApplicationEngine::quit, &app, &QGuiApplication::quit);
+	QObject::connect(view.engine(), &QQmlApplicationEngine::quit, &app,
+			 &QGuiApplication::quit);
 
-    view.setSource(QUrl("qrc:/Call.qml"));
-    view.setColor(QColor(Qt::transparent));
-    view.show();
+	view.setSource(QUrl("qrc:/Call.qml"));
+	view.setColor(QColor(Qt::transparent));
+	view.show();
 
-    return app.exec();
+	return app.exec();
 }
